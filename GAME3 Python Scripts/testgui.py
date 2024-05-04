e8bad264d766074344dcca7e4b37eac6a7edc2a1
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2024-05-04 17:29:47 +1000

    more realistic steering

--- GAME3 Python Scripts/testgui.py
+++ GAME3 Python Scripts/testgui.py
@@ -14,7 +14,7 @@ def process_steering_data(serial_port):
         data = (line.split(","))
         steering_angle_unprocessed = float(data[2]) # Angle
 
-        if(100*np.abs(steering_angle_unprocessed) < 50):
+        if(100*np.abs(steering_angle_unprocessed) < 80):
             steering_angle = 0
         else:
             steering_angle = steering_angle_unprocessed
@@ -27,13 +27,14 @@ class DotWidget(QWidget):
         super().__init__()
         self.setGeometry(100, 100, 400, 200)
         self.delta_angle = 0
-        self.pos_x = 50
+        self.pos_x = 200
         self.pos_y = 500
-        self.angle = -np.pi/2
-        self.serial_port = serial.Serial('/dev/cu.usbmodem142303', 115200)  # Adjust baudrate as per your requirement
+        self.angle = 0
+        self.steering_output = 0
+        self.serial_port = serial.Serial('COM10', 115200)  # Adjust baudrate as per your requirement
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_position)
-        self.timer.start(10)  # Update every 100 milliseconds
+        self.timer.start(10)  # Update every 10 milliseconds
     
 
     def paintEvent(self, event):
@@ -41,7 +42,7 @@ class DotWidget(QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setBrush(QColor(255, 0, 0))
         painter.drawEllipse(int(self.pos_x) - 10, int(self.pos_y) - 10, 20, 20)
-        painter.drawLine(int(self.pos_x) + int(30*np.cos(self.angle)), int(self.pos_y) + int(30*np.sin(self.angle)), int(self.pos_x), int(self.pos_y))
+        painter.drawLine(int(self.pos_x) + int(30*np.cos(self.steering_output)), int(self.pos_y) + int(30*np.sin(self.steering_output)), int(self.pos_x), int(self.pos_y))
 
     def update_position(self):
         while self.serial_port.in_waiting:
@@ -51,8 +52,9 @@ class DotWidget(QWidget):
                 # Map the value to the position on the screen
                 self.delta_angle = value
                 self.angle = self.delta_angle + self.angle
-                self.pos_x = self.pos_x + np.cos(self.angle)
-                self.pos_y = self.pos_y + np.sin(self.angle)
+                self.steering_output = self.steering_output + self.angle/10
+                self.pos_x = self.pos_x + 5*np.cos(self.steering_output)
+                self.pos_y = self.pos_y + 5*np.sin(self.steering_output)
                 self.update()  # Trigger repaint
             except ValueError:
                 print("Invalid data received from serial port:", steering_angle)
@@ -61,5 +63,5 @@ class DotWidget(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = DotWidget()
-    window.show()
+    window.showMaximized()
     sys.exit(app.exec_())
