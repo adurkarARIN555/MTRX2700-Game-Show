8e8ee77f410979bc5ac724a5edb823c56c24018d
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2024-05-14 14:26:19 +1000

    remove players

--- Integration Script/mainwindow.py
+++ Integration Script/mainwindow.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QWidget
+from PyQt5 import QtCore as qtc
 
 import game3
 
@@ -12,6 +13,9 @@ class AnotherWindow(QWidget):
     This "window" is a QWidget. If it has no parent, it
     will appear as a free-floating window as we want.
     """
+
+    submitted = qtc.pyqtSignal(str)
+
     def __init__(self, argument):
         super().__init__()
         players = argument.split("               ")
@@ -29,7 +33,26 @@ class AnotherWindow(QWidget):
         button4 = QPushButton(players[3], self)
         button4.setGeometry(100, 10, 100, 30)
 
-        
+        button1.clicked.connect(self.button1_pressed)
+        button2.clicked.connect(self.button2_pressed)
+        button3.clicked.connect(self.button3_pressed)
+        button4.clicked.connect(self.button4_pressed)
+
+    def button1_pressed(self):
+        self.submitted.emit("Player 1")
+        self.close()
+
+    def button2_pressed(self):
+        self.submitted.emit("Player 2")
+        self.close()
+
+    def button3_pressed(self):
+        self.submitted.emit("Player 3")
+        self.close()
+
+    def button4_pressed(self):
+        self.submitted.emit("Player 4")
+        self.close()
 
 
 class MainWindow(QMainWindow):
@@ -50,10 +73,15 @@ class MainWindow(QMainWindow):
         self.button.setGeometry(100, 100, 100, 30)
         self.button.clicked.connect(self.show_new_window)
 
+    def update_players(self, player_eliminated):
+        self.player_list.remove(player_eliminated)
+        self.label.setText("               ".join(self.player_list))
+
     def show_new_window(self, checked):
         if self.w is None:
             #self.w = game3.GameWindow()
             self.w = AnotherWindow("               ".join(self.player_list))
+            self.w.submitted.connect(self.update_players)
         self.w.show()
 
 if __name__ == '__main__':
