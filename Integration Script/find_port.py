032fc84f5f3bf521d0e35d608c99e5b740580464
jamessccook <160551764+jamessccook@users.noreply.github.com>
2024-05-20 10:45:06 +1000

    Comments for find_port.py

--- Integration Script/find_port.py
+++ Integration Script/find_port.py
@@ -1,4 +1,6 @@
 from serial.tools import list_ports
-port = list(list_ports.comports())
-for p in port:
+
+port = list(list_ports.comports())     # Gets a list of all of the ports
+
+for p in port:                         # Loops through all of the ports and prints out the device name of each port in use
     print(p.device)
