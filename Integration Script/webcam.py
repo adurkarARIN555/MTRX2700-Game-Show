6cb9bdb9f8ef73f8ffb51e578874395e7d38eb7a
jamessccook <160551764+jamessccook@users.noreply.github.com>
2024-05-20 11:07:34 +1000

    Comments for webcam.py

--- Integration Script/webcam.py
+++ Integration Script/webcam.py
@@ -1,3 +1,4 @@
+# Import relevant libraries and modules
 import sys
 import cv2
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
@@ -5,18 +6,19 @@ from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer, pyqtSignal
 
 class WebcamApp(QWidget):
-    submitted = pyqtSignal()
+    submitted = pyqtSignal()                                                                             # Used for syncing the integrations windows
 
     def __init__(self, argument):
         super().__init__()
         self.player_name = argument
-        self.initUI()
-        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Open the default camera
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_frame)
-        self.timer.start(100)  # Update the frame every 30 ms
+        self.initUI()                                                                                     # Initialise the user interface
+        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)                                                     # Open the default camera
+        self.timer = QTimer(self)                                                                         # Creating a timer to change the frame on
+        self.timer.timeout.connect(self.update_frame)                                                     # Connects the timer to the frame of the interface
+        self.timer.start(100)                                                                             # Starts the timer. triggering and update of the frame every 100 ms
 
     def initUI(self):
+        # User-clickable button, that once pressed, will take a photo of the current frame of the webcam
         self.image_label = QLabel(self)
         self.capture_button = QPushButton('Capture', self)
         self.capture_button.clicked.connect(self.capture_photo)
@@ -24,42 +26,41 @@ class WebcamApp(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.image_label)
         vbox.addWidget(self.capture_button)
-        
+
+        # Sets a spot for the webcam feed on the UI
         self.setLayout(vbox)
         self.setWindowTitle('Webcam Photo Capture')
         self.setGeometry(100, 100, 800, 600)
         
     def update_frame(self):
-        ret, frame = self.cap.read()
+        ret, frame = self.cap.read() 
+        
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            height, width, channel = frame.shape
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)                                                 # Converts to RGB
+            height, width, channel = frame.shape                                                           # Gets the dimensions of the frame
             step = channel * width
-            qimg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
-            self.image_label.setPixmap(QPixmap.fromImage(qimg))
+            qimg = QImage(frame.data, width, height, step, QImage.Format_RGB888)                           # Creates an image for the GUI
+            self.image_label.setPixmap(QPixmap.fromImage(qimg))                                            # Displays image
             
     def capture_photo(self):
         ret, frame = self.cap.read()
+        
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            height, width, channel = frame.shape
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)                                                 # Converts to RGB
+            height, width, channel = frame.shape                                                           # Gets the dimensions of the frame
             step = channel * width
-            qimg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
-            pixmap = QPixmap.fromImage(qimg)
-            self.image_label.setPixmap(pixmap)
-            cv2.imwrite("User Images/"+self.player_name+".png", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-            print('Photo captured and saved as captured_photo.png')
+            qimg = QImage(frame.data, width, height, step, QImage.Format_RGB888)                           # Creates an image for the GUI
+            pixmap = QPixmap.fromImage(qimg)                                                               # Converts the image to a pixmap
+            self.image_label.setPixmap(pixmap)                                                             # Displays image
+            cv2.imwrite("User Images/"+self.player_name+".png", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))    # Captures the current frame of the webcam
+            print('Photo captured and saved in User Images folder')
 
-            self.submitted.emit()
-            self.cap.release()
-            self.close()
-
-    # def closeEvent(self, event):
-    #     self.cap.release()
-    #     event.accept()
+            self.submitted.emit()                                                                          # Ensures the integration windows are synchronised
+            self.cap.release()                                                                             # Closes the webcam
+            self.close()                                                                                   # Closes the webcam window
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = WebcamApp()
-    ex.show()
+    ex = WebcamApp()                                                                                       # Create a webcam for each player
+    ex.show()                                                                                              # Display the application window
     sys.exit(app.exec_())
